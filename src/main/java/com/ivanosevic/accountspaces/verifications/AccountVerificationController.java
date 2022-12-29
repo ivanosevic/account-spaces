7cfac5dda4def49e0fa898d3edb4e01dcf8b36d2
garcia <garcia@infox.de>
2022-12-29 16:52:43 +0100

    add extra documentation for modules & clean imports

--- src/main/java/com/ivanosevic/accountspaces/verifications/AccountVerificationController.java
+++ src/main/java/com/ivanosevic/accountspaces/verifications/AccountVerificationController.java
@@ -1,8 +1,9 @@
 package com.ivanosevic.accountspaces.verifications;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AccountVerificationController {
